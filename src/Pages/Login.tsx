c4899dab07cff338838c2ce514d9efa35ee09154
lashasaata <lashasaata@gmail.com>
2024-10-10 04:30:12 -0700

    content and base structure for login component

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,5 +1,24 @@
 function Login() {
-  return;
+  return (
+    <main>
+      <img src="/wlcm.png" alt="" />
+      <h1>Welcome back</h1>
+      <section>
+        <label htmlFor="email">
+          <span>Email</span>
+          <input id="email" type="text" />
+        </label>
+        <label htmlFor="password">
+          <span>password</span>
+          <input id="password" type="password" />
+        </label>
+      </section>
+      <section>
+        <button>Sign in</button>
+        <button>Create new account</button>
+      </section>
+    </main>
+  );
 }
 
 export default Login;
