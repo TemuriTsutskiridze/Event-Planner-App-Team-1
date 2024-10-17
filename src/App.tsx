509d862eeea17c3ef232c4da561c8509150add75
lashasaata <lashasaata@gmail.com>
2024-10-17 10:41:04 -0700

    make some global types also make some changes in app.tsx also install hook-forms yup toastify

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 import {
   BrowserRouter as Router,
@@ -8,16 +9,21 @@ import {
 import Login from "./Pages/Login";
 import SignUp from "./Pages/SignUp";
 import Events from "./Pages/Events";
+import { ToastContainer } from "react-toastify";
+import { UserProvider } from "./Context/useAuth";
 
 function App() {
   return (
     <main className="w-full min-h-scroll flex flex-col items-center">
       <BrowserRouter>
-        <Routes>
-          <Route path="/Sign_in" element={<Login />} />
-          <Route path="/Sign_up" element={<SignUp />} />
-          <Route path="/events" element={<Events />} />
-        </Routes>
+        <UserProvider>
+          <Routes>
+            <Route path="/Sign_in" element={<Login />} />
+            <Route path="/Sign_up" element={<SignUp />} />
+            <Route path="/events" element={<Events />} />
+          </Routes>
+          <ToastContainer />
+        </UserProvider>
       </BrowserRouter>
     </main>
   );
