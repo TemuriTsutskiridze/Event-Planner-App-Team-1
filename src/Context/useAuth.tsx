2092132ddbd591935144707750f9969a6259f642
lashasaata <lashasaata@gmail.com>
2024-10-17 10:31:44 -0700

    feat: make context type, also make functions to handle login/registration process and saves data in localstorage, at the and it returns context provider with values and functions

---
+++ src/Context/useAuth.tsx
@@ -0,0 +1,110 @@
+import { Children, createContext, useEffect, useState } from "react";
+import { UserProfile } from "../Models/User";
+import { useNavigate } from "react-router-dom";
+import { loginAPI, registerAPI } from "../Services/AuthService";
+import { toast } from "react-toastify";
+import React from "react";
+import axios from "axios";
+
+type UserContextType = {
+  user: UserProfile | null;
+  token: string | null;
+  registerUser: (
+    email: string,
+    username: string,
+    password: string,
+    passwordRe: string
+  ) => void;
+  loginUser: (email: string, password: string) => void;
+  logout: () => void;
+  isLoggedIn: () => boolean;
+};
+
+type Props = { children: React.ReactNode };
+
+const UserContext = createContext<UserContextType>({} as UserContextType);
+
+export const UserProvider = ({ children }: Props) => {
+  const navigate = useNavigate();
+  const [token, setToken] = useState<string | null>(null);
+  const [user, setUser] = useState<UserProfile | null>(null);
+  const [isready, setIsReady] = useState(false);
+
+  useEffect(() => {
+    const user = localStorage.getItem("user");
+    const token = localStorage.getItem("token");
+    if (user && token) {
+      setUser(JSON.parse(user));
+      setToken(token);
+      axios.defaults.headers.common["Authorization"] = "Bearer " + token;
+    }
+
+    setIsReady(true);
+  }, []);
+
+  const registerUser = async (
+    email: string,
+    username: string,
+    password: string,
+    passwordRe: string
+  ) => {
+    await registerAPI(email, username, password, passwordRe)
+      .then((res) => {
+        if (res) {
+          localStorage.setItem("token", res?.data.token);
+          const userObj = {
+            userName: res?.data.userName,
+            email: res?.data.email,
+            password: res?.data.password,
+          };
+          localStorage.setItem("user", JSON.stringify(userObj));
+          setToken(res?.data.token!);
+          setUser(userObj!);
+          toast.success("Login Success!");
+          navigate("/Sign_in");
+        }
+      })
+      .catch((e) => toast.warning("Server error occured"));
+  };
+
+  const loginUser = async (email: string, password: string) => {
+    await loginAPI(email, password)
+      .then((res) => {
+        if (res) {
+          localStorage.setItem("token", res?.data.token);
+          const userObj = {
+            password: res?.data.password,
+            email: res?.data.email,
+          };
+          localStorage.setItem("user", JSON.stringify(userObj));
+          setToken(res?.data.token!);
+          setUser(userObj!);
+          toast.success("Login Success!");
+          navigate("/events");
+        }
+      })
+      .catch((e) => toast.warning("Server error occured"));
+  };
+
+  const isLoggedIn = () => {
+    return !!user;
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    setUser(null);
+    setToken("");
+    navigate("/Sign_in");
+  };
+
+  return (
+    <UserContext.Provider
+      value={{ loginUser, user, token, logout, isLoggedIn, registerUser }}
+    >
+      {isready ? children : null}
+    </UserContext.Provider>
+  );
+};
+
+export const useAuth = () => React.useContext(UserContext);
